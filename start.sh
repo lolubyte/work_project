b037266fa73782cc40a5359ab3d226f7797de70a
daboyewa <info@lolubyte.com>
2021-06-05 13:49:03 -0400

    Added secondry url

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Author Name: Daniel Aboyewa
 # email: info@lolubyte.com
-# # url: www.lbitc.com
+# url: www.lbitc.com
+# Secondry Url: www.lolubyte.com 
 /etc/rc.d/init.d/nagios start
 /usr/sbin/httpd -k start
 tail -f /var/log/httpd/access_log /var/log/httpd/error_log
