9393a7818fc63eeb9e2c2b0501ec9b73caac3d60
daboyewa <info@lolubyte.com>
2021-06-05 12:57:10 -0400

    Update Start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Author Name: Daniel Aboyewa
+# email: info@lolubyte.com
+#
 /etc/rc.d/init.d/nagios start
 /usr/sbin/httpd -k start
 tail -f /var/log/httpd/access_log /var/log/httpd/error_log
